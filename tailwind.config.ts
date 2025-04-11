3fa18b675b8c7ce30d4dc5728905dfc7f9cff2a5
MauriJC <xaviermauricio33@gmail.com>
2025-04-11 14:40:59 -0300

    chore: installed framer-motion

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        extend: {
+            content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+
+            colors: {
+                cream: 'rgba(245,238,230,0.5)',
+            },
+            fontFamily: {
+
+            }
+        }
+    }
+}
