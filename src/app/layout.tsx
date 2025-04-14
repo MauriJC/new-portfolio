c6ecf00b890f40cf50fcf7086b0bf05f0902cc07
MauriJC <xaviermauricio33@gmail.com>
2025-04-14 16:24:46 -0300

    feat: improved metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,17 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
-  title: "Mauricio Chaile",
+  title: "Mauricio Chaile | Fullstack Developer",
+  description: "I'm Mauricio, a software engineer, and this is my personal portfolio showcasing web development projects and my journey in tech.",
+  keywords: ["Mauricio Chaile", "Fullstack Developer", "Web Developer", "Portfolio", "Software Engineer", "Web Development", "React", "Next.js", "JavaScript"],
+  openGraph: {
+    title: "Mauricio Chaile | Fullstack Developer",
+    description: "Check out my portfolio showcasing my projects and skills as a software engineer.",
+    siteName: "Mauricio Chaile",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
