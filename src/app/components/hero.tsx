790820fdcc51c1aa8292ca0f3e5076cb99f22494
MauriJC <xaviermauricio33@gmail.com>
2025-04-27 23:18:35 -0300

    refactor: modify styles for mobile

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -20,16 +20,14 @@ const Hero = () => {
             <Backgrounds scrollYProgress={scrollYProgress} />
             {/* Contenido */}
             <section className="relative h-screen z-10 lg:flex flex-row items-center" id="about">
-                <div className="h-[50%] w-[50%] flex flex-col justify-end h-screen">
+                <div className="h-[50%] w-[50%] flex flex-col justify-end lg:h-screen">
                     <motion.h1
                         initial={{ opacity: 0, x: 50 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         whileHover={{ translateX: 50 }}
                         transition={{ duration: 1 }}
                         viewport={{ amount: 0.3 }}
-                        className="text-[20rem] lg:text-[25vh] font-[tusker-semibold] mb-[-10vh]"
-                        drag
-                        dragConstraints={containerRef}
+                        className="text-[12rem]  mb-[-5rem] lg:text-[25vh] lg:mb-[-7rem] font-[tusker-semibold]"
                     >
                         FULLSTACK
                     </motion.h1>
@@ -40,15 +38,13 @@ const Hero = () => {
                         whileHover={{ translateX: 50 }}
                         transition={{ duration: 1 }}
                         viewport={{ amount: 0.3 }}
-                        className="text-[20rem] lg:text-[25vh] font-[tusker-semibold] mb-0"
-                        drag
-                        dragConstraints={containerRef}
+                        className="text-[12rem] lg:text-[25vh] font-[tusker-semibold] mb-0"
                     >
                         DEVELOPER
                     </motion.h1>
                 </div>
 
-                <div className="w-[50%] md:h-screen flex flex-col justify-center mr-6">
+                <div className="w-[50%] h-[50%] md:h-screen flex flex-col justify-center mr-6">
                     <motion.div
                         initial={{ opacity: 0, y: 50 }}
                         whileInView={{ opacity: 1, y: 0 }}
@@ -57,8 +53,8 @@ const Hero = () => {
                         className="text-3xl font-[neue-medium] mt-[40%] text-right"
                     >
                         <p>I AM MAURICIO, A FULLSTACK DEVELOPER</p>
-                        <p> BASED IN ARGENTINA. I AM STARTING</p>
-                        <p>MY JOURNEY AS SOFTWARE ENGINEER.</p>
+                        <p> BASED IN ARGENTINA. WHO IS</p>
+                        <p>DOING HIS JOURNEY AS SOFTWARE ENGINEER.</p>
                         <p>I LOVE EFFICIENT AND ROBUST</p>
                         <p> SOFTWARE DESIGN. I LOVE PLAYING,</p>
                         <p>MILANESA AND PROGRAMMING.</p>
@@ -69,18 +65,6 @@ const Hero = () => {
                             CONTACT ME!
                         </a>
                     </motion.div>
-
-                    {/* <motion.div
-                        initial={{ opacity: 0, y: 100 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                        transition={{ duration: 1, ease: 'easeInOut' }}
-                        viewport={{ amount: 0.4, once: true }}
-                        className="text-white text-3xl"
-                    >
-                        ¡Hola! Soy una caja animada 😄
-                    </motion.div> */}
                 </div>
             </section>
 
