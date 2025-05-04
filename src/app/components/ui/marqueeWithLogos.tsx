16377b81d8314dd2da475aa010fcbd5189f85322
MauriJC <xaviermauricio33@gmail.com>
2025-05-03 23:49:50 -0300

    feat; add Marquee With Logos component

--- src/app/components/ui/marqueeWithLogos.tsx
+++ src/app/components/ui/marqueeWithLogos.tsx
@@ -1,42 +1,51 @@
 import {
-    ReactLogo, TailwindLogo, NextjsLogo, JSLogo, TSLogo, NodeLogo, PostgreSQLLogo, PrismaLogo, DjangoLogo, GitLogo
+  ReactLogo,
+  TailwindLogo,
+  NextjsLogo,
+  JSLogo,
+  TSLogo,
+  NodeLogo,
+  PostgreSQLLogo,
+  PrismaLogo,
+  DjangoLogo,
+  GitLogo,
 } from './logos';
 
 interface Technology {
-    name: string;
-    Logo: React.FC<React.SVGProps<SVGSVGElement>>;
+  name: string;
+  Logo: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
 // Array de tecnologías
 const technologies: Technology[] = [
-    { name: "React", Logo: ReactLogo },
-    { name: "TailwindCSS", Logo: TailwindLogo },
-    { name: "Next.js", Logo: NextjsLogo },
-    { name: "JavaScript", Logo: JSLogo },
-    { name: "TypeScript", Logo: TSLogo },
-    { name: "Node.js", Logo: NodeLogo },
-    { name: "PostgreSQL", Logo: PostgreSQLLogo },
-    { name: "Prisma", Logo: PrismaLogo },
-    { name: "Django", Logo: DjangoLogo },
-    { name: "Git", Logo: GitLogo },
+  { name: 'React', Logo: ReactLogo },
+  { name: 'TailwindCSS', Logo: TailwindLogo },
+  { name: 'Next.js', Logo: NextjsLogo },
+  { name: 'JavaScript', Logo: JSLogo },
+  { name: 'TypeScript', Logo: TSLogo },
+  { name: 'Node.js', Logo: NodeLogo },
+  { name: 'PostgreSQL', Logo: PostgreSQLLogo },
+  { name: 'Prisma', Logo: PrismaLogo },
+  { name: 'Django', Logo: DjangoLogo },
+  { name: 'Git', Logo: GitLogo },
 ];
 
 const MarqueeWithLogos: React.FC = () => {
-    return (
-        <div className="marquee">
-            <div
-                className="marquee-track"
-                style={{ '--num-items': technologies.length * 2 } as React.CSSProperties}
-            >
-                {[...technologies, ...technologies].map((tech, idx) => (
-                    <div key={idx} className="marquee-item flex flex-col gap-4">
-                        <p className="font-bold text-xs lg:text-base">{tech.name}</p>
-                        <tech.Logo />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="marquee">
+      <div
+        className="marquee-track"
+        style={{ '--num-items': technologies.length * 2 } as React.CSSProperties}
+      >
+        {[...technologies, ...technologies].map((tech, idx) => (
+          <div key={idx} className="marquee-item flex flex-col gap-4">
+            <p className="text-xs font-bold lg:text-base">{tech.name}</p>
+            <tech.Logo />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MarqueeWithLogos;
