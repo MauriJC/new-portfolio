2f50d81643b6bea0e8717467b1acc01ca57677b3
MauriJC <xaviermauricio33@gmail.com>
2025-05-03 23:45:31 -0300

    chore: format file

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,39 +4,48 @@ import { useState } from 'react';
 import { scrollToId } from '../utils/scrollToId'; // ajusta el path según tu estructura
 
 const NavBar = () => {
-    const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState('');
 
-    return (
-        <div className="fixed top-0 left-0 w-full z-50 bg-transparent px-12 pt-4 pb-4 flex justify-between items-center border-b border-[rgba(245,238,230,0.5)] backdrop-blur-sm">
-            <div>
-                <a href="#" className="font-[underdog] text-xl">
-                    Mauricio Chaile
-                </a>
-                <span className="font-[neue-medium] ml-8">FULLSTACK DEVELOPER</span>
-            </div>
+  return (
+    <div className="fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-[rgba(245,238,230,0.5)] bg-transparent px-12 pt-4 pb-4 backdrop-blur-sm">
+      <div>
+        <a href="#" className="font-[underdog] text-xl">
+          Mauricio Chaile
+        </a>
+        <span className="ml-8 font-[neue-medium]">FULLSTACK DEVELOPER</span>
+      </div>
 
-            <div className="flex justify-between text-lg">
-                <a
-                    onClick={() => { scrollToId('about', -80); setSelected('about') }}
-                    className={`font-[tusker] ml-1 cursor-pointer  ${selected == 'about' ? 'line-through' : ''}`}
-                >
-                    about-
-                </a>
-                <a
-                    onClick={() => { scrollToId('projects', -0); setSelected('projects') }}
-                    className={`font-[tusker] ml-1 cursor-pointer  ${selected == 'projects' ? 'line-through' : ''}`}
-                >
-                    projects-
-                </a>
-                <a
-                    onClick={() => { scrollToId('contact', -0); setSelected('contact') }}
-                    className={`font-[tusker] ml-1 cursor-pointer  ${selected == 'contact' ? 'line-through' : ''}`}
-                >
-                    contact
-                </a>
-            </div>
-        </div>
-    );
+      <div className="flex justify-between text-lg">
+        <a
+          onClick={() => {
+            scrollToId('about', -80);
+            setSelected('about');
+          }}
+          className={`ml-1 cursor-pointer font-[tusker] ${selected == 'about' ? 'line-through' : ''}`}
+        >
+          about-
+        </a>
+        <a
+          onClick={() => {
+            scrollToId('projects', -0);
+            setSelected('projects');
+          }}
+          className={`ml-1 cursor-pointer font-[tusker] ${selected == 'projects' ? 'line-through' : ''}`}
+        >
+          projects-
+        </a>
+        <a
+          onClick={() => {
+            scrollToId('contact', -0);
+            setSelected('contact');
+          }}
+          className={`ml-1 cursor-pointer font-[tusker] ${selected == 'contact' ? 'line-through' : ''}`}
+        >
+          contact
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
