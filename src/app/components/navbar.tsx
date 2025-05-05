223396a1c8697940ce83c978793c6cc71060cea0
MauriJC <xaviermauricio33@gmail.com>
2025-05-04 22:29:19 -0300

    feat: add animation to navbar and experience section scroll to

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,12 +2,19 @@
 
 import { useState } from 'react';
 import { scrollToId } from '../utils/scrollToId'; // ajusta el path según tu estructura
+import { motion } from 'framer-motion';
 
 const NavBar = () => {
   const [selected, setSelected] = useState('');
 
   return (
-    <div className="fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-[rgba(245,238,230,0.5)] bg-transparent px-12 pt-4 pb-4 backdrop-blur-sm">
+    <motion.nav
+      className="fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b border-[rgba(245,238,230,0.5)] bg-transparent px-12 pt-4 pb-4 backdrop-blur-sm"
+      initial={{ y: -50 }}
+      animate={{ y: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <div>
         <a href="#" className="font-[underdog] text-xl">
           Mauricio Chaile
@@ -36,6 +43,15 @@ const NavBar = () => {
         </a>
         <a
           onClick={() => {
+            scrollToId('experience', -0);
+            setSelected('experience');
+          }}
+          className={`ml-1 cursor-pointer font-[tusker] ${selected == 'experience' ? 'line-through' : ''}`}
+        >
+          experience
+        </a>
+        <a
+          onClick={() => {
             scrollToId('contact', -0);
             setSelected('contact');
           }}
@@ -44,7 +60,7 @@ const NavBar = () => {
           contact
         </a>
       </div>
-    </div>
+    </motion.nav>
   );
 };
 
